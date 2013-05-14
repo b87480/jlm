9bae01815975ab40859aeef77cb1a3858f397c89
Joshua Whitney <jwhitney114@fullsail.edu>
2013-05-14 17:41:30 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JLM::Application.config.secret_token = '8537b6a2ad7e456fc074bb8806b6b5ff89f95ee6f432ac2a08123eef4f09bc13fdf42f3c811fe94ee6da547f9addbe7490cbb7d65fd2b873aef9690a0f10906c'
